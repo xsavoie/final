b649fc483b8c9f85d866b6584bd645d49fdbafb0
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-11-19 11:20:40 -0500

    add seeds for tables

--- backend/db/seeds/isabel_seeds.sql
+++ backend/db/seeds/isabel_seeds.sql
@@ -1,3 +1,23 @@
+-- seeds for users
+INSERT INTO users (email, username, password) VALUES ('alice@gmail.com', 'asdbri1', 'password');
+INSERT INTO users (email, username, password) VALUES ('bob@gmail.com', 'asggb343', 'password');
+INSERT INTO users (email, username, password) VALUES ('lydia@gmail.com', 'a34rfsd', 'password');
+INSERT INTO users (email, username, password) VALUES ('evan@gmail.com', 'a345sdf', 'password');
+INSERT INTO users (email, username, password) VALUES ('david@gmail.com', 'ahtsd34', 'password');
+
+-- seeds for categories
+
+INSERT INTO categories (name) VALUES ('Secret');
+INSERT INTO categories (name) VALUES ('Story');
+INSERT INTO categories (name) VALUES ('Question');
+
+-- seeds for confessions
+
+INSERT INTO confessions (user_id, category_id, content, created_at) VALUES (1, 1, 'something something content', '2018-02-12T08:00:00.000Z');
+INSERT INTO confessions (user_id, category_id, content, created_at) VALUES (2, 2, 'something something content', '2018-02-12T08:00:00.000Z');
+INSERT INTO confessions (user_id, category_id, content, created_at) VALUES (3, 3, 'something something content', '2018-02-12T08:00:00.000Z');
+INSERT INTO confessions (user_id, category_id, content, created_at) VALUES (4, 1, 'something something content', '2018-02-12T08:00:00.000Z');
+INSERT INTO confessions (user_id, category_id, content, created_at) VALUES (5, 2, 'something something content', '2018-02-12T08:00:00.000Z');
 
 -- seeds for likes table 
 
@@ -14,4 +34,3 @@ INSERT INTO comments (user_id, confession_id, content, created_at) VALUES (1, 3,
 INSERT INTO comments (user_id, confession_id, content, created_at) VALUES (2, 4, 'Well done, for coming clean', '2018-02-12T08:00:00.000Z');
 INSERT INTO comments (user_id, confession_id, content, created_at) VALUES (2, 1, 'You are awesome', '2018-02-12T08:00:00.000Z');
 INSERT INTO comments (user_id, confession_id, content, created_at) VALUES (3, 2, 'Thank you for sharing', '2018-02-12T08:00:00.000Z');
-
