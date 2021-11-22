2ed6c7c407ae2d61a2a7fdeb0a2898edea65df14
Xavier Savoie <xaviersavoie@gmail.com>
2021-11-22 17:13:05 +0000

    can now create new comments for confession and like. Still missing state logic

--- front-end/src/components/Comments/CommentsList.jsx
+++ front-end/src/components/Comments/CommentsList.jsx
@@ -4,35 +4,16 @@ import "./CommentsList.scss"
 
 export default function CommentsList(props) {
 
-  const commentArray = [
-    {
-      "content": "You are awesome",
-      "user_id": 2
-    },
-    {
-      "content": "test comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "user_id": 1
-    },
-    {
-      "content": "test comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "user_id": 2
-    },
-    {
-      "content": "test comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "user_id": 3
-    }
-  ];
-
-
   const parsedComments = props.comments.map((comment) => (
     <CommentsListItem
+      key={comment.id}
       content={comment.content}
     />
   ));
 
   return (
     <section className="comments__list">
-      <CommentForm/>
+      <CommentForm confessionId={props.confessionId} />
       <ul>{parsedComments}</ul>
     </section>
   )
