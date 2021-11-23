e61930706fc0087c7d2d2988c641983859e4e9b9
Xavier Savoie <xaviersavoie@gmail.com>
2021-11-23 03:05:24 +0000

    wrapped socket in useEffect

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import { React, useEffect, useState, useMemo } from 'react';
 import socketClient from "socket.io-client";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserContext } from './components/contexts/UserContext';
 
 import './App.css';
@@ -21,20 +21,25 @@ const SERVER = "http://localhost:3000";
 
 
 function App() {
-  
+
   const [confessions, setConfessions] = useState([]);
   const [user, setUser] = useState(null)
+  const [showForm, setShowForm] = useState(false)
 
-  const socket = socketClient("http://localhost:3001", {
-    withCredentials: true,
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    }
-  });
+  useEffect(() => {
+    const socket = socketClient("http://localhost:3001", {
+      withCredentials: true,
+      extraHeaders: {
+        "my-custom-header": "abcd"
+      }
+    });
+  
+    socket.on('connection', () => {
+      console.log(`I'm connected with the back-end`);
+    });
+    
+  }, [])
 
-  socket.on('connection', () => {
-    console.log(`I'm connected with the back-end`);
-});
 
 
   // return(
@@ -44,8 +49,7 @@ function App() {
 
 
 
-  const [showForm, setShowForm] = useState(false)
-  
+
   // const providerValue = useMemo(() => ({user, setUser}), [user, setUser])
 
   // useEffect(() => {
@@ -82,24 +86,24 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div className="App">
-      {/* <header className="App-header"> */}
-      <Top user={user} setUser={setUser} showForm={showForm} setShowForm={setShowForm}/>
-      {/* <h1>Confessions</h1><br /> */}
-       {/* <UserContext.Provider value={providerValue}> */}
-      <Routes>
-       
-          <Route path="/login" element={<LoginForm setUser={setUser}/>}></Route>
-          <Route path="/register" element={<RegisterForm setUser={setUser}/>}></Route>
-          <Route path="/home" element={<ConfessionList confessionsToParse={confessions}setConfessions={setConfessions}/>}></Route>
-       
+      <div className="App">
+        {/* <header className="App-header"> */}
+        <Top user={user} setUser={setUser} showForm={showForm} setShowForm={setShowForm} />
+        {/* <h1>Confessions</h1><br /> */}
+        {/* <UserContext.Provider value={providerValue}> */}
+        <Routes>
+
+          <Route path="/login" element={<LoginForm setUser={setUser} />}></Route>
+          <Route path="/register" element={<RegisterForm setUser={setUser} />}></Route>
+          <Route path="/home" element={<ConfessionList confessionsToParse={confessions} setConfessions={setConfessions} />}></Route>
+
         </Routes>
-      {showForm && <ConfessionForm confessions={confessions} setConfessions={setConfessions}/>}
-      <ConfessionList confessionsToParse={confessions} setConfessions={setConfessions}/>
+        {showForm && <ConfessionForm confessions={confessions} setConfessions={setConfessions} />}
+        <ConfessionList confessionsToParse={confessions} setConfessions={setConfessions} />
         {/* </UserContext.Provider> */}
       </div>
-      </BrowserRouter>
-   
+    </BrowserRouter>
+
   );
 }
 
