bdd4b54bd51b9e1a3ba5e8e6bb394eebbd8663f5
Xavier Savoie <xaviersavoie@gmail.com>
2021-11-26 18:21:40 +0000

    fixed navbar navigation issues

--- front-end/src/components/navbar/Top.jsx
+++ front-end/src/components/navbar/Top.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from "react"
 import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { UserContext } from "../contexts/UserContext";
-
 
 export default function Top(props) {
 
@@ -13,72 +12,52 @@ export default function Top(props) {
     sessionStorage.clear();
   }
 
+  const handleRouteChange = (category) => {
+    props.setConfessionFeed(category);
+    props.setPageToDisplay(1);
+    props.setShowRegister(false);
+    props.setShowLogin(false);
+    props.setShowForm(false)
+  }
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand
           href="#home"
-          onClick={() => {
-            props.setShowLogin(false);
-            props.setShowRegister(false);
-            props.setPageToDisplay(1)
-            props.setConfessionFeed("recent")
-          }}
+          onClick={() => handleRouteChange("recent")}
         >
           <Link to="/" >classified</Link>
         </Navbar.Brand>
-        {/* <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      </Nav> */}
-
         <div className="">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <NavDropdown title="Category" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/" onClick={() => {
-                  props.setConfessionFeed(1);
-                  props.setPageToDisplay(1)
-                  <Navigate />
-                  // return <Redirect to="/" />
-                }}
-                >Secret
+                <NavDropdown.Item href="/" onClick={() => handleRouteChange(1)}
+                ><Link to="/" >Secret</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => {
-                  props.setConfessionFeed(2);
-                  props.setPageToDisplay(1)
-                }}
-                >Story
+                <NavDropdown.Item onClick={() => handleRouteChange(2)}
+                ><Link to="/" >Story</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => {
-                  props.setConfessionFeed(3);
-                  props.setPageToDisplay(1)
-                }}
-                >Question
+                <NavDropdown.Item onClick={() => handleRouteChange(3)}
+                ><Link to="/" >Question</Link>
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item onClick={() => {
-                  props.setConfessionFeed("recent");
-                  props.setPageToDisplay(1)
-                }}
-                >Most recent
+                <NavDropdown.Item onClick={() => handleRouteChange("recent")}
+                ><Link to="/" >Most recent</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => {
-                  props.setConfessionFeed("popular");
-                  props.setPageToDisplay(1)
-                }}
-                >Most popular
+                <NavDropdown.Item onClick={() => handleRouteChange("popular")}
+                ><Link to="/" >Most popular</Link>
                 </NavDropdown.Item>
               </NavDropdown>
-              {/* confess form for when on / page --> brings down form */}
-              {user.id && <Nav.Link onClick={() => props.setShowForm(!props.showForm)}>Confess</Nav.Link>}
-              {/* confess form for when on other page than / --> brings to / and opens form */}
-              {/* {user.id && <Nav.Link onClick={() => props.setShowForm(!props.showForm)}>Confess</Nav.Link>} */}
+              {user.id && !props.showForm && <Nav.Link onClick={() => props.setShowForm(true)}><Link to="/" >Confess</Link></Nav.Link>}
+              {user.id && props.showForm && <Nav.Link onClick={() => props.setShowForm(false)}>Confess</Nav.Link>}
               {user.id && <Nav.Link href="/chat"><Link to="/chat">Chat</Link></Nav.Link>}
               {user.id && <NavDropdown title={user.email} id="basic-nav-dropdown">
                 <NavDropdown.Item href="/profile">Go to profile</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="/" onClick={() => handleLogout()}>Logout</NavDropdown.Item>
+                <NavDropdown.Item href="/" onClick={() => handleLogout(props.showForm)}>Logout</NavDropdown.Item>
               </NavDropdown>}
               {!user.id &&
                 <Nav.Link
